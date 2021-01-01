9338891a796df7b268d6bc91a901c179fdf1c624
nhoxbinhduongvn <binkool2811324422@gmail.com>
2021-01-01 20:19:39 +0700

    Updates

---
+++ precache-manifest.50e457f5bdb5a871957c2f4a243608f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a4c3e7583f010bf7fe0df43632bf7e",
+    "url": "/vinh-flix/index.html"
+  },
+  {
+    "revision": "856930c590188978c8aa",
+    "url": "/vinh-flix/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b2d7ddc224edf33f3f52",
+    "url": "/vinh-flix/static/css/main.29221621.chunk.css"
+  },
+  {
+    "revision": "856930c590188978c8aa",
+    "url": "/vinh-flix/static/js/2.5b9647b7.chunk.js"
+  },
+  {
+    "revision": "7c8c498c154c5c7fc14cf0ad148613b4",
+    "url": "/vinh-flix/static/js/2.5b9647b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d7ddc224edf33f3f52",
+    "url": "/vinh-flix/static/js/main.4487e9b8.chunk.js"
+  },
+  {
+    "revision": "5cdf36efc1d970a9023a",
+    "url": "/vinh-flix/static/js/runtime-main.60aa8ac9.js"
+  },
+  {
+    "revision": "19aeb3b3e521ca3585188ccf00a9026d",
+    "url": "/vinh-flix/static/media/VinhFlix.19aeb3b3.png"
+  }
+]);
